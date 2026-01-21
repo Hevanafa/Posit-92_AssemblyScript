9cb4bcb92e99d1d31a275d816c7ca2c1a4dafdaf
Hevanafa <dhevansafa4@gmail.com>
2026-01-21 14:57:57 +0700

    changed types to Pascal types

---
+++ experimental/units/pascal_compat.ts
@@ -0,0 +1,10 @@
+export type pointer = usize;
+export type PByte = usize;
+export type PByteArray = usize;
+
+export type ShortInt = i8;
+export type Byte = u8;
+export type SmallInt = i16;
+export type Word = u16;
+export type LongInt = i32;
+export type LongWord = u32;
--- experimental/units/vga.ts
+++ experimental/units/vga.ts
@@ -1,38 +1,40 @@
+import { Byte, LongWord, PByteArray, pointer, SmallInt, Word } from "./pascal_compat";
+
 let
-  vgaWidth: i16 = 0,
-  vgaHeight: i16 = 0,
-  bufferSize: i32 = 0,
-  surface: usize = 0;
+  vgaWidth: Word = 0,
+  vgaHeight: Word = 0,
+  bufferSize: LongWord = 0,
+  surface: PByteArray = 0;
 
 //@ts-ignore
 @external("env", "vgaFlush")
 export declare function vgaFlush(): void;
 //@ts-check
 
-export function initVideoMem(width: i16, height: i16, startAddr: usize): void {
+export function initVideoMem(width: Word, height: Word, startAddr: pointer): void {
   vgaWidth = width;
   vgaHeight = height;
   surface = startAddr;
-  bufferSize = i32(vgaWidth) * i32(vgaHeight) * 4;
+  bufferSize = <LongWord>vgaWidth * vgaHeight * 4;
   memory.fill(surface, 0, bufferSize);
 }
 
-export function getSurfacePtr(): usize {
+export function getSurfacePtr(): pointer {
   return surface;
 }
 
-function unsafePset(x: i16, y: i16, colour: u32): void {
-  const offset: u32 = (u32(y) * vgaWidth + x) * 4;
+function unsafePset(x: SmallInt, y: SmallInt, colour: LongWord): void {
+  const offset = (<LongWord>y * vgaWidth + x) * 4;
 
   // ARGB to RGBA
-  store<u8>(surface + offset, u8(colour >> 16 & 0xFF));
-  store<u8>(surface + offset + 1, u8(colour >> 8 & 0xFF));
-  store<u8>(surface + offset + 2, u8(colour & 0xFF));
-  store<u8>(surface + offset + 3, u8(colour >> 24 & 0xFF));
+  store<Byte>(surface + offset, <Byte>(colour >> 16 & 0xFF));
+  store<Byte>(surface + offset + 1, <Byte>(colour >> 8 & 0xFF));
+  store<Byte>(surface + offset + 2, <Byte>(colour & 0xFF));
+  store<Byte>(surface + offset + 3, <Byte>(colour >> 24 & 0xFF));
 }
 
-export function cls(colour: u32): void {
-  for (let b: i16 = 0; b < vgaHeight; b++)
-    for (let a: i16 = 0; a < vgaWidth; a++)
+export function cls(colour: LongWord): void {
+  for (let b: Word = 0; b < vgaHeight; b++)
+    for (let a: Word = 0; a < vgaWidth; a++)
       unsafePset(a, b, colour);
 }
