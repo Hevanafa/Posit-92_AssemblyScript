5f36d16742ed36f43e066bc934e38953b9aa01a1
Hevanafa <dhevansafa4@gmail.com>
2026-01-23 11:51:14 +0700

    added register image ref

--- experimental/units/img_ref.ts
+++ experimental/units/img_ref.ts
@@ -1,4 +1,4 @@
-import { LongWord, PByte, SmallInt } from "./pascal_compat";
+import { LongInt, LongWord, PByte, SmallInt } from "./pascal_compat";
 
 class TImageRef {
   width: SmallInt;
@@ -10,7 +10,7 @@ class TImageRef {
     this.width = 0;
     this.height = height;
     this.allocSize = 0;
-    this.dataPtr = null;
+    this.dataPtr = 0;
   }
 }
 
@@ -24,4 +24,24 @@ export enum SprFlips {
   SprFlipBoth = SprFlipHorizontal | SprFlipVertical
 }
 
-let imageRefs: Array<TImageRef>;
+const MaxImageRefs = 256;
+
+// Index 0 is unused
+const imageRefs = new StaticArray<TImageRef>(MaxImageRefs + 1);
+
+export function registerImageRef(imgHandle: LongInt, dataPtr: PByte, w: SmallInt, h: SmallInt): void {
+  const idx = findEmptyImageRefSlot();
+
+  if (idx < 0) panicHalt("Image ref pool is full!");
+
+  imageRefs[imgHandle].width = w;
+  imageRefs[imgHandle].height = h;
+  imageRefs[imgHandle].allocSize = <LongWord>w * h * 4;
+  imageRefs[imgHandle].dataPtr = dataPtr;
+}
+
+
+// Initialisation
+for (let a=0; a<=MaxImageRefs; a++)
+  imageRefs[a] = new TImageRef();
+
