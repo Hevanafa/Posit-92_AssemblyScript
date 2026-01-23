ac4060a7e3803f0fbdad6e88c5f029f2b4dee340
Hevanafa <dhevansafa4@gmail.com>
2026-01-23 15:35:07 +0700

    added null check in isImageSet

--- experimental/units/img_ref.ts
+++ experimental/units/img_ref.ts
@@ -70,18 +70,17 @@ export function getImagePtr(imgHandle: LongInt): TImageRef {
 export function isImageSet(imgHandle: LongInt): boolean {
   if (imgHandle <= 0) return false;
 
-  return imageRefs[imgHandle].allocSize > 0
+  return (imageRefs[imgHandle].dataPtr != null) && (imageRefs[imgHandle].allocSize > 0)
 }
 
 // Assuming image isn't nil & the bounds are known
 export function unsafeSprPget(image: TImageRef, x: SmallInt, y: SmallInt): LongWord {
-  let offset: LongWord;
-  offset = (<LongWord>x + y * image.width) * 4;
+  const offset = (<LongWord>x + y * image.width) * 4;
 
-  return (load<Byte>(image.dataPtr + offset + 3) << 24)
-    | (load<Byte>(image.dataPtr + offset) << 16)
-    | (load<Byte>(image.dataPtr + offset + 1) << 8)
-    | (load<Byte>(image.dataPtr + offset + 2))
+  return (load<Byte>(image.dataPtr! + offset + 3) << 24)
+    | (load<Byte>(image.dataPtr! + offset) << 16)
+    | (load<Byte>(image.dataPtr! + offset + 1) << 8)
+    | (load<Byte>(image.dataPtr! + offset + 2))
 }
 
 
