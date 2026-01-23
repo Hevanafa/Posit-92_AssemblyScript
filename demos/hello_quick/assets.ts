e97ddacd1381c04047324b16e4164bcd04526bb1
Hevanafa <dhevansafa4@gmail.com>
2026-01-23 12:02:41 +0700

    added WasmGetMem

--- demos/hello_quick/assets.ts
+++ demos/hello_quick/assets.ts
@@ -3,6 +3,6 @@ import { LongInt } from "../../experimental/units/pascal_compat";
 export let
   imgCursor: LongInt;
 
-export function setImgCursor(imgHandle: LongInt) {
+export function setImgCursor(imgHandle: LongInt): void {
   imgCursor = imgHandle
 }
--- experimental/units/wasm_heap.ts
+++ experimental/units/wasm_heap.ts
@@ -1,4 +1,10 @@
+import { LongWord, pointer } from "./pascal_compat";
+
 /**
  * For compatibility with Pascal
  */
 export function initHeap(heapStart: u32, heapSize: u32): void {}
+
+export function WasmGetMem(bytes: LongWord): pointer {
+  return __new(bytes, 0)
+}
